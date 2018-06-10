ffa71936f597e33a35575f9e8b88ac89cbb4aa51
imran0801 <imu.imran123@gmail.com>
2018-06-10 12:36:49 +0530

    First java commit

---
+++ src/com/github/sampleprograms/Sample_1.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.github.sampleprograms;
+
+/**
+ * @author lenovo
+ *
+ */
+public class Sample_1 {
+
+	/**
+	 Adding two numbers
+	 */
+	public static void main(String[] args) {
+		int a=10, b=20, c = 0;
+		System.out.println(c = a+b);
+		
+
+	}
+
+}
